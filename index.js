7d729c463e3dca0e36ca8592102e88f15340ae21
nithy anand <nithyk666@gmail.com>
2024-03-18 04:40:17 +0000

    reusable code updated

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const GENERIC_DOMAINS = "('samplemail.com','samplemail.co.uk','examplemailbox.com'";
-
-function getDomain(email) {
-    let cleanEmail = `trim(${email})`
-    const domain = `substr(${cleanEmail}, strpos(${cleanEmail}, '@') + 1)`;
-    return `case
-            when ${domain} in ${common.GENERIC_DOMAINS} then ${cleanEmail}
-            when ${domain} = "othermailbox.com" then "other.com"
-            when ${domain} = "mailbox.com" then "mailbox.global"
-            when ${domain} = "support.postman.com" then "postman.com"
-            else ${domain}
-          end`;
+function getDomain(table) {    
+    return `SELECT * FROM ${table}`;
 }
 
 module.exports = {
